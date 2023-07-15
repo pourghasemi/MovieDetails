8b5d3095510500b026d77391be62e1b47a8ac04c
shiva pourghasemi <shiva.pourghasemi@yahoo.com>
2023-07-15 14:51:51 +0200

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const path = require("path");
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  // images: {
+  //   domains: ["m.media-amazon.com"],
+  // },
+};
+
+module.exports = nextConfig;
